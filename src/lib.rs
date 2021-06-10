7734be2eb03419a46a5ba0745608824a2f1b41e5
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-10 15:50:12 +0900

    docs: add the top-level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! A wrapper of UEFI functions.
+//!
+//! This crate is intended to prvide safe and stable wrappers of UEFI functions. The users of this
+//! crate does not have to write any unsafe codes or utilize nightly features.
+//!
+//! # Usage
+//!
+//! Specify [`Handle`] and [`SystemTable`] as the parameters of main function.
+//!
+//! ```rust,no_run
+//! # fn main(){}   // See: https://stackoverflow.com/questions/48830213/how-to-doctest-a-function-that-uses-a-trait-from-a-crate
+//! // You are supposed to use the `#![no_std]` attribute. In the example, the attribute is
+//! // omitted, however, because it requires `eh_personatily` by default.
+//!
+//! use core::writeln;
+//! use core::fmt::Write;
+//!
+//! #[no_mangle]
+//! pub extern "win64" fn efi_main(h: uefi_wrapper::Handle, mut st: uefi_wrapper::SystemTable) -> ! {
+//!     let mut con_out = st.con_out();
+//!
+//!     writeln!(con_out, "hello world");
+//!
+//!     loop {}
+//! }
+//! ```
+
 #![no_std]
 #![deny(unsafe_op_in_unsafe_fn)]
 
