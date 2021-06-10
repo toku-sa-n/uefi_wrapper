6dfdeaed4dcc0c07f2c487f48fa08b5ed3f4d4dc
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-10 22:14:12 +0900

    docs(protocols): add the docs of `Protocol`

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -5,6 +5,12 @@ pub mod media;
 
 use r_efi::efi;
 
+/// Types that represent UEFI protocols.
+///
+/// # Safety
+///
+/// Types that implement this trait must have the same structure as the protocol interface.
 pub unsafe trait Protocol {
+    /// The GUID of the protocol.
     const GUID: efi::Guid;
 }
